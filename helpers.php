e9a8b40af80c473ff3d3cd4087b4664c6c9592a6
comingdemon_git <comingdemon@sina.com>
2020-12-22 22:46:54 +0800

    New adminHtml function

--- helpers.php
+++ helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-use \Demon\Library\Admin;
+use Demon\AdminLaravel\Html;
 
 /**
  * 运维后台URL路径
@@ -16,4 +16,50 @@ use \Demon\Library\Admin;
 function adminUrl($path = null, $parameters = [], $secure = null)
 {
     return url(config('admin.web.path') . ($path ? '/' . $path : ''), $parameters, $secure);
+}
+
+/**4
+ * 运维后台快速生成HTML片段
+ * @return array|Html|object|string
+ * @copyright 魔网天创信息科技
+ * @author    ComingDemon
+ */
+function adminHtml()
+{
+    $html = Html::instance();
+    //  获取参数
+    $parm = func_get_args();
+    //  如果有参数
+    if (isset($parm[0])) {
+        switch ($parm[0]) {
+            case 'entity':
+                return $html->entity($parm[1] ?? '');
+                break;
+            case 'card':
+                return $html->card($parm[1] ?? '', $parm[2] ?? '');
+                break;
+            case 'button':
+                return $html->button($parm[1] ?? '', $parm[2] ?? [], $parm[3] ?? 'a', $parm[4] ?? 'btn-xs btn-primary');
+                break;
+            case 'input':
+                return $html->input($parm[1] ?? '', $parm[2] ?? [], $parm[3] ?? 'text', $parm[4] ?? '');
+                break;
+            case 'batch':
+                return $html->batch($parm[1] ?? [], $parm[3] ?? 'i-checks');
+                break;
+            case 'fast':
+                return $html->fast($parm[1] ?? '', $parm[2] ?? [], $parm[3] ?? 'span', $parm[4] ?? '');
+                break;
+            case 'image':
+                return $html->image($parm[1] ?? '', $parm[2] ?? 50, $parm[3] ?? 50, $parm[4] ?? '');
+                break;
+            case 'search':
+                return $html->search($parm[1] ?? null, $parm[2] ?? '', $parm[3] ?? []);
+                break;
+            default:
+                return $html;
+        }
+    }
+
+    return $html;
 }
