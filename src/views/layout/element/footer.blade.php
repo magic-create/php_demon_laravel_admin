4b1b8e84fb926ca659e7dc53b6b39e9f87e79704
comingdemon_git <comingdemon@sina.com>
2021-02-19 15:45:29 +0800

    Optimize some details

--- src/views/layout/element/footer.blade.php
+++ src/views/layout/element/footer.blade.php
@@ -1,3 +1,3 @@
 @section('footer')
-    <div>© 2010 - 2021 Demon Frame <span class="d-none d-sm-inline-block">-  Crafted with <i class="fa fa-heart text-danger"></i> by ComingDemon.</span></div>
+    <div>© 2010 - {{date('Y')}} Demon Frame <span class="d-none d-sm-inline-block">-  Crafted with <i class="fa fa-heart text-danger"></i> by ComingDemon.</span></div>
 @endsection
